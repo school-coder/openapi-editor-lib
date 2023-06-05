949e83ef8962c0fe6592c89b225cdd89fc38a3b8
Nagarajan Sivaraman <naga4042@gmail.com>
2023-06-05 11:12:23 +0530

    adding editor and editor diff comonent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {OpenapiEditorLibModule} from '../../projects/openapi-editor-lib/src/lib/openapi-editor-lib.module';
+import {HeaderComponent} from "../components/header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-      OpenapiEditorLibModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        OpenapiEditorLibModule,
+        HeaderComponent,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/components/editor-diff/editor-diff.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-editor-diff',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './editor-diff.component.html',
+  styleUrls: ['./editor-diff.component.css']
+})
+export class EditorDiffComponent {
+
+}
